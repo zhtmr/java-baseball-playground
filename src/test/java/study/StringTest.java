8a3a575469e04c186fd034a986f331ea742e2dfb
csw <susu199037@gmail.com>
2024-05-25 20:41:13 +0900

    test: step0

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +12,31 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] split = "1,2".split(",");
+        assertThat(split).contains("1", "2");
+        String[] split1 = "1".split(",");
+        assertThat(split1).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String s = "(1,2)";
+        String substring = s.substring(1, s.length() -1);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("범위를 벗어나면 StringIndexOutOfBoundsException 발생한다.")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            String s = "abc";
+            char c = s.charAt(999);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 999");
+    }
+
+
 }
